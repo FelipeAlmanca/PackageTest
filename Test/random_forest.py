4a41512279d256f391a146bf3b0c968555c6db6b
FelipeAlmanca <54955176+FelipeAlmanca@users.noreply.github.com>
2022-04-04 22:54:14 -0300

    Update random_forest.py

--- Test/random_forest.py
+++ Test/random_forest.py
@@ -4,8 +4,6 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-import numpy as np 
-import pandas as pd 
 
 #Lendo arquivo
 
